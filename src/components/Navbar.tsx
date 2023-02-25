5d5e1a8b8ae8255db29d62df301014a07b966c19
shashankpatil125 <shashankpatil125@gmail.com>
2023-02-25 18:58:31 +0530

    sign in

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -16,15 +16,23 @@ function Navbar() {
             Acquire
           </span>
         </a>
+
         <div className="flex md:order-2">
           {loginStatus ? (
-            <></>
+            <button
+              type="button"
+              className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            >
+              LogOut
+            </button>
           ) : (
             <button
               type="button"
               className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
             >
-              Login
+              <Link href="/login">
+                <p>Login</p>
+              </Link>
             </button>
           )}
           <button
