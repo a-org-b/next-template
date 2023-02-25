b24aa8bf797588202ef6a7d2c3bce9894767ee90
kunal232i <kingawale23@gmail.com>
2023-02-25 06:00:47 +0000

    showcase initial commit

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,14 +6,14 @@ function Navbar() {
   return (
     <nav className="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-900">
       <div className="container flex flex-wrap items-center justify-between mx-auto">
-        <a href="https://flowbite.com/" className="flex items-center">
+        <a href="#" className="flex items-center">
           <img
             src="https://flowbite.com/docs/images/logo.svg"
             className="h-6 mr-3 sm:h-9"
             alt="Flowbite Logo"
           />
           <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
-            Site Name
+            Acquire
           </span>
         </a>
         <div className="flex md:order-2">
@@ -35,7 +35,7 @@ function Navbar() {
             aria-expanded="false"
           >
             <span className="sr-only">Open main menu</span>
-            <p>aquireCompany</p>
+            <p>Acquire-C</p>
           </button>
         </div>
         <div
