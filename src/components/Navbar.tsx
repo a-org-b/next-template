73865528f37bed5152ca3e09fab577c27a87c39c
thisisommore <ommore501@gmail.com>
2023-02-26 15:05:45 +0000

    Navbar: route to showcase on logo

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -13,17 +13,18 @@ function Navbar() {
   return (
     <nav className="bg-white backdrop-blur-2xl border-b-2 border-gray-500 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-900 fixed top-0 w-full">
       <div className="container flex flex-wrap items-center justify-between mx-auto">
-        <a href="#" className="flex items-center">
-          <img
-            src="https://flowbite.com/docs/images/logo.svg"
-            className="h-6 mr-3 sm:h-9"
-            alt="Flowbite Logo"
-          />
-          <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
-            Acquire
-          </span>
-        </a>
-
+        <Link href="/showcase">
+          <div className="flex items-center">
+            <img
+              src="https://flowbite.com/docs/images/logo.svg"
+              className="h-6 mr-3 sm:h-9"
+              alt="Flowbite Logo"
+            />
+            <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
+              Acquire
+            </span>
+          </div>
+        </Link>
         <div className="flex md:order-2">
           {loginStatus ? (
             <button
@@ -38,14 +39,14 @@ function Navbar() {
               Log Out
             </button>
           ) : (
-            <button
-              type="button"
-              className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-            >
-              <Link href="/login">
+            <Link href="/login">
+              <button
+                type="button"
+                className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              >
                 <p>Login</p>
-              </Link>
-            </button>
+              </button>
+            </Link>
           )}
           <button
             data-collapse-toggle="navbar-cta"
